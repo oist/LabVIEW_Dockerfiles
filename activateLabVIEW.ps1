38e7a6c57785993227459e24c381cedd7ad89e65
Christian Butcher <chrisb2244@gmail.com>
2022-06-16 18:27:15 +0900

    Add a script to handle file paths for the LabVIEW activation. This should be called with the serial number and version required.

---
+++ activateLabVIEW.ps1
@@ -0,0 +1,10 @@
+param (
+  [string]$Version,
+  [string]$SerialNumber
+)
+
+Start-Process "C:\Program Files (x86)\National Instruments\Shared\License Manager\Bin\nilmUtil.exe" `
+  -ArgumentList "-s", "-activate", "LabVIEW_PDS_PKG", "-version", $Version, "-serialnumber", $SerialNumber `
+  -Wait
+
+Get-Content "C:\ProgramData\National Instruments\License Manager\Licenses\LabVIEW_PDS_PKG*"
