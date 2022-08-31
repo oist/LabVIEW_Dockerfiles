d5fb02a45d4fe1180ce476f9a9967eda48b87cea
Christian Butcher <chrisb2244@gmail.com>
2022-08-31 10:08:37 +0900

    Build the agent JAR files and plugins directory into the build image. Include MD5 checking vs the remote host in the startup process in case of updates - in that case, there will be no gain here until the image is rebuilt. Place the download of these files at the end of the dockerfiles to prevent reinstalling all of LabVIEW in that case.

--- go-agent.ps1
+++ go-agent.ps1
@@ -8,25 +8,25 @@ Add-Content -Path .\config\autoregister.properties -Value ("agent.auto.register.
 Add-Content -Path .\config\autoregister.properties -Value ("agent.auto.register.elasticAgent.pluginId="+$env:GO_EA_AUTO_REGISTER_ELASTIC_PLUGIN_ID)
 Get-Content -Path .\config\autoregister.properties
 
-function download_file {
-	param (
-		[string]$remotePath,
-		[string]$localPath
-	)
-	$url = $env:GO_EA_SERVER_URL+$remotePath
-	Invoke-WebRequest $url -OutFile $localPath
-}
-
 function get_checksums {
 	$url = $env:GO_EA_SERVER_URL+"/admin/latest-agent.status"
 	(Invoke-WebRequest $url -UseBasicParsing).Headers
 }
 
-# Download files from the server/elastic host/something?
-$ProgressPreference = 'SilentlyContinue'
-download_file -remotePath '/admin/agent' -localPath 'agent.jar'
-download_file -remotePath '/admin/agent-plugins.zip' -localPath 'agent-plugins.zip'
-download_file -remotePath '/admin/tfs-impl.jar' -localPath 'tfs-impl.jar'
+function checkAndUpdate {
+	param(
+		[string]$localPath,
+		[string]$remotePath,
+		[string]$remoteChecksum
+	)
+	if ((Test-Path $localPath -PathType Leaf) -and ((Get-FileHash -Algorithm MD5 $localPath).Hash -eq $remoteChecksum.ToUpper())) {
+		Write-Host $localPath " is up-to-date"
+	} else {
+		Write-Host $localPath " requires update..."
+		$url = $env:GO_EA_SERVER_URL+$remotePath
+		Invoke-WebRequest $url -OutFile $localPath
+	}
+}
 
 # Get MD5 values from the server. These might be used for authentication/registration?
 $checksums = get_checksums
@@ -35,7 +35,14 @@ $tfs_md5 = $checksums.'TFS-SDK-Content-MD5'
 $plugins_md5 = $checksums.'Agent-Plugins-Content-MD5'
 $agent_launcher_md5 = $checksums.'Agent-Launcher-Content-MD5'
 
-echo "Launching the GoCD Agent, and waiting for it to exit?"
+# Download files from the server/elastic host if not up-to-date in the base image.
+# This will typically mean slower starts after updating the GoCD server, until the images are rebuilt.
+$ProgressPreference = 'SilentlyContinue'
+checkAndUpdate -localPath 'agent.jar' -remotePath '/admin/agent' -remoteChecksum $agent_md5
+checkAndUpdate -localPath 'agent-plugins.zip' -remotePath '/admin/agent-plugins.zip' -remoteChecksum $plugins_md5
+checkAndUpdate -localPath 'tfs-impl.jar' -remotePath '/admin/tfs-impl.jar' -remoteChecksum $tfs_md5
+
+Write-Host "Launching the GoCD Agent, and waiting for it to exit?"
 $RUN_CMD = "java --% -Dcruise.console.publish.interval=10 -Xms128m -Xmx256m -Djava.security.egd=file:/dev/./urandom -Dagent.plugins.md5=$plugins_md5 -Dagent.binary.md5=$agent_md5 -Dagent.launcher.md5=$agent_launcher_md5 -Dagent.tfs.md5=$tfs_md5 -jar agent.jar -serverUrl $env:GO_EA_SERVER_URL"
 
 Invoke-Expression $RUN_CMD
